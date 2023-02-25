df41e581fceea65e935601f7d86666c0898ae229
Katrina L Lyman <77120233+lymankatrina@users.noreply.github.com>
2023-02-25 15:52:11 -0700

    Added functions to calculator

--- calculator.js
+++ calculator.js
@@ -1,18 +1,28 @@
 let calculator = {
-  sum() {
-    return this.a + this.b;
-  },
-  
-  mul() {
-    return this.a * this.b;
-  },
-  
-  read() {
-    this.a = +prompt('a?', 0);
-    this.b = +prompt('b?', 0);
-  }
+	addition() {
+		return this.a + this.b;
+	},
+
+	multiply() {
+		return this.a * this.b;
+	},
+
+	subtract() {
+		return this.a - this.b;
+	},
+
+	divide() {
+		return this.a / this.b;
+	},
+
+	read() {
+		this.a = +prompt("a?", 0);
+		this.b = +prompt("b?", 0);
+	},
 };
 
 calculator.read();
-alert( calculator.sum() );
-alert( calculator.mul() );
+alert(calculator.addition());
+alert(calculator.multiply());
+alert(calculator.subtract());
+alert(calculator.divide());
